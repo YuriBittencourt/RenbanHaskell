94d4986389e4c2bb30efe8d68154fec2b1e843f4
Gabriel Wesz <gabwesz@outlook.com>
2020-11-20 01:56:06 -0300

    Adiciona função que verifica se há sequencia em uma lista

--- resolvedor.hs
+++ resolvedor.hs
@@ -1,4 +1,8 @@
-module Resolvedor (resolve) where
+--module Resolvedor (resolve) where
+
+import Matriz
+import Data.List (sort)
+import System.Exit
 
 resolve :: Int -> String
 resolve x =
@@ -6,3 +10,25 @@ resolve x =
     "Teste"
   else
     "NOT teste"
+
+
+
+-- -=-=-=-=-=-=-=-=- Aqui estão as funções auxiliares -=-=-=-=-=-=-=-=-
+
+
+-- Confere se existe uma sequência de números em uma lista
+ehSequencia :: [Int] -> Bool
+ehSequencia [] = True
+ehSequencia lista =
+  if (sort lista) == [((sort lista)!!0)..(last(sort lista))] then
+    True
+  else
+    False
+
+
+
+-- Main para testes
+
+main = do
+  print(ehSequencia   [1, 3, 2, 4])
+  print(ehSequencia [1, 2, 5, 6])
